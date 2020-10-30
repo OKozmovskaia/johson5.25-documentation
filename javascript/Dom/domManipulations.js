d0cb655f499a6ee4852ef04ff222488175f4ea65
OKozmovskaia <olga.kozmovskaia@gmail.com>
2020-10-30 11:48:52 +0100

    Update DOM-manipulations, add info about nav

--- javascript/Dom/domManipulations.js
+++ javascript/Dom/domManipulations.js
@@ -36,6 +36,21 @@ document.querySelector().getAttribute();  // for read attribute
 document.querySelector().setAttribute();  // for write or re-write attribute
 document.querySelector().hasAttribute();  // for check presence of attribute
 
+// For navigation of DOM-objects you can to use parent/ child/ and sibling nodes.
+// This property automatic built-in in every-objects of DOM.
+// And this is very useful when you need to write access to unexisting object, which
+// for example create user in future, because you can suggest where in structure of your
+// page this object will be
+
+// For nested elements <table> - <tr> - <td>/ <ul> - <li>
+elem.childNodes[0] === elem.firstChild;
+elem.childNodes[elem.childNodes.length - 1] === elem.lastChild;
+document.body.parentNode === document.documentElement;
+
+// For neighbor elements <li>/ <td>/ <tr>
+document.body.previousSibling;
+document.body.nextSibling;
+
 // You can find DOM-spec here: https://dom.spec.whatwg.org
 
 
